5a45296e0c18b55077630a8e9bf489d319194afb
Herbert <herbe06@gmail.com>
2021-08-12 22:26:55 -0300

    organizando

--- ed2.c
+++ ed2.c
@@ -1,13 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define tam 100
 
-int colisao = 0;
+void criaDados(int m){
+    FILE *arq = fopen("entradas.bin","wb");
+    int aux;
+    for(int i=0;i<tam;i++){
+        aux = rand() % m;
+        fwrite(&aux,sizeof(int),sizeof(int),arq);
+    }
+    fclose(arq);
+}
+
+int hash(int chave, int m) {
+    return chave % m;
+}
+
+void insere(int chave){
+    FILE *arq = fopen("tabela.bin","wb");
+    fseek(arq,hash(chave,tam),SEEK_SET);
+    fwrite(&chave,sizeof(int),sizeof(int),arq);
+    fclose(arq);
+}
+
+int busca(int chave){
+    FILE *arq = fopen("tabela.bin","rb");
+    int aux;
+    fseek(arq,hash(chave,tam),SEEK_SET);
+    fread(&aux,sizeof(int),sizeof(int),arq);
+
+    if(aux == chave){
+        return 1;
+    }
+    else{
+        for(int i = hash(chave,tam);i<100;i++){
+            fseek(arq,i,SEEK_SET);
+            fread(&aux,sizeof(int),sizeof(int),arq);
+            if(aux == chave){
+                return 1;
+            }
+        }
+    }
+    fclose(arq);
+    return 0;
+}
+
 
 int main(void) {
 
-    criaDados(100,100); //criando arquivo com entradas aleatorias
-
+    criaDados(100);
+    insere(101);
+    printf("%d\n",busca(9999));
 
     return EXIT_SUCCESS;
 }
